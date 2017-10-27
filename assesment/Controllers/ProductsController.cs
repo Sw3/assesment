bf9898b433491950286f4d4fe8bbfa48b697f58e
miguel <miguelgao@hotmail.com>
2017-10-27 01:44:08 -0500

    tests finished

--- assesment/Controllers/ProductsController.cs
+++ assesment/Controllers/ProductsController.cs
@@ -18,8 +18,59 @@ namespace assesment.Controllers
         // GET: Products
         public ActionResult Index()
         {
-            
-            return View(db.Products.ToList());
+            if ((System.Web.HttpContext.Current.Session["DataAccess"]) == null)
+            {
+                System.Web.HttpContext.Current.Session["DataAccess"] = "DB";
+                Session["products"] = "";
+                Session["products"] = "";
+
+            }
+            if ((System.Web.HttpContext.Current.Session["DataAccess"]) == "DB")
+                    return View(db.Products.ToList());
+            else
+            {
+                string[] ProductParams = Session["products"].ToString().Split(',');
+           
+                List<Product> products = new List<Product>();
+                if (ProductParams.Count() > 1)
+                {
+                    for (int i = 0; i <= ProductParams.Count(); i++)
+                    {
+                        try
+                        {
+                            Product product = new Product();
+                            product.productNumber = int.Parse(ProductParams[i]);
+                            i++;
+                            product.title = ProductParams[i];
+                            i++;
+                            product.price = Double.Parse(ProductParams[i]);
+                            i++;
+                            products.Add(product);
+                        }
+                        catch (Exception e)
+                        {
+                            break;
+                        }
+                    }
+
+                }
+                return View(products);
+            }
+                
+
+        }
+        // GET: Products
+        public ActionResult changeDataSource()
+        {
+            if ((System.Web.HttpContext.Current.Session["DataAccess"]) == "DB")
+            {
+                (System.Web.HttpContext.Current.Session["DataAccess"]) = "Local";
+            }else
+            {
+                (System.Web.HttpContext.Current.Session["DataAccess"]) = "DB";
+            }
+
+            return RedirectToAction("Index");
         }
 
         // GET: Products/Details/5
@@ -57,9 +108,20 @@ namespace assesment.Controllers
             Product product = new Product { productNumber = int.Parse(productNumber), title=title, price= price, category= db.ProductCategories.Where( f => f.ID == Categories).ToList()[0] };
             if (ModelState.IsValid)
             {
+                if ((System.Web.HttpContext.Current.Session["DataAccess"]) == "Local")
+                {
+                    System.Web.HttpContext.Current.Session["products"] += product.productNumber + ",";
+                    System.Web.HttpContext.Current.Session["products"] += product.title+",";
+                    System.Web.HttpContext.Current.Session["products"] += product.price + ",";
+                    System.Web.HttpContext.Current.Session["products"] += product.categories + ",";
+                }
+                else
+                {
                 var r = ViewBag.Categories;
                 db.Products.Add(product);
                 db.SaveChanges();
+                }
+                
                 return RedirectToAction("Index");
             }
             return View(product);
