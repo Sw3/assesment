25e776f0951f51e46388efe1c8a113d3b2fbbeff
miguel <miguelgao@hotmail.com>
2017-10-26 21:27:26 -0500

    EntityFramework working correctly -views initialized

---
+++ assesment/Controllers/ProductCategoriesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using assesment.DataAccessLayer;
+using assesment.Models;
+
+namespace assesment.Controllers
+{
+    public class ProductCategoriesController : Controller
+    {
+        private WebShopContext db = new WebShopContext();
+
+        // GET: ProductCategories
+        public ActionResult Index()
+        {
+            return View(db.ProductCategories.ToList());
+        }
+
+        // GET: ProductCategories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductCategory productCategory = db.ProductCategories.Find(id);
+            if (productCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productCategory);
+        }
+
+        // GET: ProductCategories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductCategories/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,CategoryName")] ProductCategory productCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductCategories.Add(productCategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productCategory);
+        }
+
+        // GET: ProductCategories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductCategory productCategory = db.ProductCategories.Find(id);
+            if (productCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productCategory);
+        }
+
+        // POST: ProductCategories/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,CategoryName")] ProductCategory productCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productCategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(productCategory);
+        }
+
+        // GET: ProductCategories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductCategory productCategory = db.ProductCategories.Find(id);
+            if (productCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productCategory);
+        }
+
+        // POST: ProductCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProductCategory productCategory = db.ProductCategories.Find(id);
+            db.ProductCategories.Remove(productCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
